0abf3a31512545e980497592674b02f5496acebd
Radhika <129591553+rs010101@users.noreply.github.com>
2024-08-24 14:48:07 +0530

    Add files via upload

---
+++ TodoList/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:todo/TODOLIST/todo_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDo App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const TodoListApp(),
+    );
+  }
+}
+
+
